2f0bfa1d36da3a972249be51f4927787d82b43bb
Scott Jehl <scottjehl@gmail.com>
2014-07-10 17:22:07 -0500

    merge

--- example/js/touch.js
+++
@@ -1,8 +0,0 @@
-// NOTE: the real contents of this file would actually do something useful.
-// for demo purposes, it logs a message to the page.
-(function( $ ){
-	// when dom is ready...
-	$(function(){
-		$( "<p>generalenhancements.js was loaded because touch events are supported.</p>" ).appendTo( "body" );
-	});
-})( wrap );
