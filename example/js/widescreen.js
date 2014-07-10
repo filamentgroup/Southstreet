34405f29306ba967c255448f1ba1624efa66e211
Scott Jehl <scottjehl@gmail.com>
2014-07-10 17:20:42 -0500

    remove old files

--- example/js/widescreen.js
+++
@@ -1,8 +0,0 @@
-// NOTE: the real contents of this file would actually do something useful.
-// for demo purposes, it logs a message to the page.
-(function( $ ){
-	// when dom is ready...
-	$(function(){
-		$( "<p>widescreen.js was loaded  because the screen width is greater than 500px.</p>" ).appendTo( "body" );
-	});
-})( wrap );
